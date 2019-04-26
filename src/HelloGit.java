1b94f2ab83386adfdd577e1dd283325caae52520
nick_xu <nick_xu007@163.com>
2019-04-26 14:08:57 +0800

    测试git2

---
+++ src/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * @Desc 测试git在idea中的使用
+ * @Author Nick_Xu
+ * @Date 2019/4/26 13:58
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("测试git在idea中的使用");
+    }
+}
